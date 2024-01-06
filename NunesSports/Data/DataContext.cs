0583c98452445e9d940e33618a33bbe113e7c240
Ramon Vicente <ramon.silva.1396106@sga.pucminas.br>
2024-01-06 00:12:38 -0300

    First Configurations

---
+++ NunesSports/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NunesSports.Shared.Entities;
+using System.ComponentModel;
+
+namespace NunesSports.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
